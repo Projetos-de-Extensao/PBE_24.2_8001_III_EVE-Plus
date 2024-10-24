4605a7d9ab17facb4faad94936f79f57cfeb286f
João Gabriel Tasca Barroso de Assis <jg@assis.net.br>
2024-10-24 11:36:19 -0300

    Fazendo o SRC(criando arquivos para fazer nosso projeto do IQuirium)
    - Criando pasta src
    - Pasta de projeto(IQuiriumProject)
    - Pasta de aplicacoes(content_app-seria o nosso member get member)
    - Criando a API

---
+++ src/content_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from content_app import models 
+
+admin.site.register(models.Content)#Pra aparecer a classe criada no Django server
---
+++ src/content_app/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from .models import Content
+from .serializers import ContentSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class ContentViewSet(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
