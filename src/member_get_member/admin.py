d6aee61d19621fdbcc7f93bfead2a69a22a667fc
Zecademonho <jg@assis.net.br>
2024-10-25 21:38:22 -0300

    Finalizando a criacao do convite automatico
    1- Mudanca no admin.py para que ele identifique que o usuario logado seja relacionado com a classe member
    2- Exlude na admin.py, para que os campos expecificados nao aparecao para preenchimento, tem que ser automatico, o remetente ser o usuario logado e o status ser pendente
    3- Arrumando a class Convite pra gerar link automatico corretamente

--- src/member_get_member/admin.py
+++ src/member_get_member/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from member_get_member import models
+from .models import Convite
 
+class ConviteAdmin(admin.ModelAdmin):
+    # Oculta os campos no Django Admin
+    exclude = ('userRemetente','status',)
+
+    def save_model(self, request, obj, form, change):
+        # Define o userRemetente como o usuário logado, se não estiver definido
+        if not obj.userRemetente_id:
+            obj.userRemetente = request.user.member
+        obj.save()
+
+# Registra os modelos no Django Admin
 admin.site.register(models.Member)
-admin.site.register(models.Convite)
+admin.site.register(Convite, ConviteAdmin)
--- src/member_get_member/models.py
+++ src/member_get_member/models.py
@@ -1,8 +1,11 @@
 from typing import Any
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 import uuid
-from datetime import datetime, timedelta
+from datetime import timedelta
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 
@@ -22,35 +25,52 @@ class Convite(models.Model):
         ('Aceito', 'Aceito'),
         ('Expirado', 'Expirado'),
     ]
-    #UserRemetente - ja eh preenchido automaticamente identificandoo o user que ja esta logado
-     #usuario logado faz o envio do convite, como obter o id de identiicacao do usuario
     userRemetente = models.ForeignKey(Member, related_name='convites', on_delete=models.CASCADE)
     userDestinatario = models.EmailField(unique= True)
-    link = models.CharField(max_length=255, unique=True)
-    #   Status - nao aparece no django adimin e eh criado automaticamente como pendente, porem quando o email for cadastrado mudara para aceito e 
+    link = models.CharField(blank=True,max_length=255, unique=True)
+    #   Status - quando o email for cadastrado mudara para aceito e 
     # quando passar a data de expiracao que pode ser e 1 semana mudara para expirado
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pendente')
     data_envio = models.DateTimeField(auto_now_add=True)
 
-    def criar_convite(self):
-        pass
-
-    def gerarLink(self):
-        self.link = str(uuid.uuid4())
-        #self.data_expiracao = datetime.now() + timedelta(days=7)
-        self.save()
-        return f"http://iquirium.com/cadastro/{self.link}"
-
     def __str__(self):
-        return self.userDestinatario +" - "+self.link
+        return f"Convite para {self.userDestinatario} - Status: {self.status}"
 
-"""
     def save(self, *args, **kwargs):
+        # Gera um link automaticamente ao salvar, se ainda não tiver sido gerado
+        if self.link:
+            self.link = f"http://iquirium.com/cadastro/{self.link}"
         if not self.link:
-            self.gerarLink()
+            self.link = f"http://iquirium.com/cadastro/{str(uuid.uuid4())}"
         super().save(*args, **kwargs)
 
+    def registrar_aceitacao(self):
+        """
+        Marca o convite como 'Aceito' e concede recompensa ao remetente.
+        """
+        if self.status == 'Pendente' and self.validar_link():
+            self.status = 'Aceito'
+            self.save()
+            # Conceder recompensa ao remetente
+            self.userRemetente.recompensas += 1
+            self.userRemetente.save()
+
+    def validar_link(self):
+        """
+        Verifica se o convite ainda está válido (dentro do prazo de 7 dias).
+        """
+        validade = self.data_envio + timedelta(days=7)
+        if timezone.now() > validade:
+            self.status = 'Expirado'
+            self.save()
+            return False
+        return True
+
+
+"""
+
 class Sistema(models.Model):
+
     def conceder_recompensa(self, member, convite):
         if convite.status == 'Aceito':
             member.recompensas += 1
@@ -58,17 +78,4 @@ class Sistema(models.Model):
         else:
             pass
 
-    def registrar_aceitacao(self, convite):
-        if convite.status == 'Pendente':
-            convite.status = 'Aceito'
-            convite.save()
-        else:
-            pass
-
-    def validar_link(self, convite):
-        if convite.data_expiracao < datetime.now():
-            convite.status = 'Expirado'
-            convite.save()
-        else:
-            pass
 """
