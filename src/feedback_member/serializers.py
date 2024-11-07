d46fc847f77f9b4f331a75fbdbfd398715ffb281
João Gabriel Tasca Barroso de Assis <jg@assis.net.br>
2024-11-07 11:41:11 -0300

    serializer do feedback

--- src/feedback_member/serializers.py
+++ src/feedback_member/serializers.py
@@ -10,4 +10,16 @@ class MemberSerializer(serializers.ModelSerializer):
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
-        fields = '__all__'
+        fields = ['member', 'feedback', 'tipo', 'data']
+        read_only_fields = ['member','data']
+
+    def create(self, validated_data):
+        request = self.context.get('request', None)
+        if request and hasattr(request, 'user'):
+            user = request.user
+            try:
+                member = Member.objects.get(user=user)
+            except Member.DoesNotExist:
+                member = Member.objects.create(user=user)
+            validated_data['member'] = member
+        return Feedback.objects.create(**validated_data)
